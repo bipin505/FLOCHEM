fd70f7f3dcda3dce14b082efc565d8250995d972
bipin505 <41504003+bipin505@users.noreply.github.com>
2020-12-31 12:39:24 +0530

    folder layout commit
    folder layout commit

--- flochem-app/src/app/app.module.ts
+++ flochem-app/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgImageSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
