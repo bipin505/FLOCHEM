aa0f951e7a221b07b1bba59ec9d92c5957991a67
bipin505 <41504003+bipin505@users.noreply.github.com>
2021-01-10 20:35:35 +0530

    mobile view initial commit
    mobile view initial commit

--- flochem-app/src/app/components/header/header.component.ts
+++ flochem-app/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-header',
@@ -7,13 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 el:HTMLElement;
+faBars:any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.faBars = faBars;
   }
   scroll(id){
     this.el = document.getElementById(id);
     this.el.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 
+  }
+  openNav(){
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
   }
 }
