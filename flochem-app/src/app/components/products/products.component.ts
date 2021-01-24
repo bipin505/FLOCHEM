98fb6016c57b9d494101e3e5583e05d477750ea6
bipin505 <41504003+bipin505@users.noreply.github.com>
2021-01-24 22:39:52 +0530

    mobile changes

--- flochem-app/src/app/components/products/products.component.ts
+++ flochem-app/src/app/components/products/products.component.ts
@@ -19,21 +19,24 @@ export class ProductsComponent implements OnInit {
       'name': 'PU & Rubber Products',
       'desc': 'Flochem is known for it’s customization of products based on customer’s need with it’s expertise in multiple sizes of PU mat panels, PU spray nozzles, PU wheels, rubber impellers and more.Also we can assist our clients with multiple other spare parts related to Mineral / Sand washing plants.',
       'path': 'image1',
-      'items': ['Customized PU Mat Panels', 'PU Spray Nozels', 'PU wheels']
+      'items': ['Customized PU Mat Panels', 'PU Spray Nozels', 'PU wheels'],
+      'image': 'assets/images/product1.jpg'
     },
     {
       'id': '2',
       'name': 'Motors, Pumps & it’s spares',
       'desc': 'We have tied up with several Motor and Pump manufacturers to cater to our customer’s need accordingly. Compatible products like Impellers and other Spares with best quality can also be provided.',
       'path': 'image2',
-      'items': ['Customized PU Mat Panels', 'PU Spray Nozels', 'PU wheels']
+      'items': ['Branded Pumps and Motors', 'Rubber and Metal Impellers', 'Gear Box'],
+      'image': 'assets/images/product2.jpg'
     },
     {
       'id': '3',
       'name': 'Dosing systems',
       'desc': 'Dosing systems are a basic need for multiple operating plants requiring fixed dosage of flocculants or other chemicals. Normally small industries opt for manual mixing and dosing of the chemicals which usually leads to wastage of chemicals and improper dosage in varying quantities.We have thus developed some simple model with easy mixing technique which enables uniform mixing and is pocket friendly to the customers seeking economical solution.On the other hand we also have fully automatic dosing system which can handle itself completely with just the push of a button (To be programmed once as per requirement).We can make customized systems too based on customer’s need.',
       'path': 'image3',
-      'items': ['Customized PU Mat Panels', 'PU Spray Nozels', 'PU wheels']
+      'items': ['Fully Automatic System','Semi Automatic System','Manual System'],
+      'image': 'assets/images/Dosing system_Edit.jpg'
     }];
 
     this.products = [
@@ -52,7 +55,7 @@ export class ProductsComponent implements OnInit {
       {
         'id': '3',
         'name': 'Soda ash & Baking soda',
-        'desc': 'We deal into multiple brands of Sodium Carbonate (Soda ash) and Sodium Bi-Carbonate (Baking soda) as per client’s requirement. Both products are available in Food grade too with our brand FC 301 (Food grade Sodium Carbonate) being a popular choice among bottled water clients.',
+        'desc': 'We deal in multiple brands of Sodium Carbonate (Soda ash) and Sodium Bi-Carbonate (Baking soda) as per client’s requirement. Both products are available in Food grade too with our brand FC 301 (Food grade Sodium Carbonate) being a popular choice among bottled water clients.',
         'image': 'assets/images/Sodaash.jpg'
       }
     ]
